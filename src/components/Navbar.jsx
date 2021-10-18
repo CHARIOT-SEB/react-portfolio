529a597d36c6418dfbe340ef982b65240792133a
CHARIOT_SEB <sebboyse@gmail.com>
2021-10-18 19:08:32 +0100

    navbar refactor for flexbox

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,10 @@
-
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-
 	return (
 		<div>
 			<header className='header' id='header'>
-				<nav className='nav-bar' id='nav-bar'>
+	
 					<div className='final-logo'>
 						<h1 className='sebastian'>Sebastian</h1>
 						<h2 className='boyse'>Boyse</h2>
@@ -14,46 +12,48 @@ const Navbar = () => {
 						<h3 className='logo-tagline'>RESPONSIVE WEB & SOFTWARE DEVELOPMENT</h3>
 					</div>
 
-					<div className='yellow-nav-block'></div>
+					<div className='yellow-nav-block'>
+						<ul className='nav-links'>
+							<li>
+								<NavLink to='/' className='nav-link' id='nav-home'>
+									Home
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									activeClassName='nav-link-bold'
+									to='/projects/'
+									className='nav-link'
+									id='nav-portfolio'
+								>
+									Projects
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									activeClassName='nav-link-bold'
+									to='/work/'
+									className='nav-link'
+									id='nav-work'
+								>
+									Work
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									activeClassName='nav-link-bold'
+									to='/contact/'
+									className='nav-link'
+									id='nav-contact'
+								>
+									Contact
+								</NavLink>
+							</li>
+						</ul>
+					</div>
 
-					<ul className='nav-links'>
-						<li>
-							<NavLink to='/' className='nav-link' id='nav-home'>
-								Home
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								activeClassName='nav-link-bold'
-								to='/projects/'
-								className='nav-link'
-								id='nav-portfolio'
-							>
-								Projects
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								activeClassName='nav-link-bold'
-								to='/work/'
-								className='nav-link'
-								id='nav-work'
-							>
-								Work
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								activeClassName='nav-link-bold'
-								to='/contact/'
-								className='nav-link'
-								id='nav-contact'
-							>
-								Contact
-							</NavLink>
-						</li>
-					</ul>
-				</nav>
+					
+
 			</header>
 		</div>
 	);
